247e46e4f47d3e43f44b0a1da3fb8d60f27e9f06
Kirill <defuntitled@gmail.com>
2024-05-24 01:45:17 +0300

    change generator

--- datasets/analyze/generate_params.py
+++ datasets/analyze/generate_params.py
@@ -5,19 +5,19 @@ import pathlib
 nmus = []
 
 for k in range(50):
-    nmus.append((random.randint(2,20), random.randint(1,2000), random.randint(1,2000)))
+    nmus.append((random.randint(2,20), random.randint(0,100), random.randint(1,2000)))
 
 for k in range(50):
-    nmus.append((random.randint(20,40), random.randint(2000,4000), random.randint(2000,4000)))
+    nmus.append((random.randint(20,40), random.randint(0,100), random.randint(2000,4000)))
 
 for k in range(50):
-    nmus.append((random.randint(40,60), random.randint(4000,6000), random.randint(4000,6000)))
+    nmus.append((random.randint(40,60), random.randint(0,100), random.randint(4000,6000)))
 
 for k in range(50):
-    nmus.append((random.randint(60,80), random.randint(6000,8000), random.randint(6000,8000)))
+    nmus.append((random.randint(60,80), random.randint(0,100), random.randint(6000,8000)))
 
 for k in range(50):
-    nmus.append((random.randint(80,100), random.randint(8000,10000), random.randint(8000,10000)))
+    nmus.append((random.randint(80,100), random.randint(0,100), random.randint(8000,10000)))
 
 with open(os.path.join(pathlib.Path(__file__).parent.resolve(), "input_params.txt"), "w") as f:
     for i in nmus:
