632c6a4646120ac5ec05719e3821b6b1c929f18b
даниил харламов <metallpeace@gmail.com>
2024-05-25 03:21:48 +0300

    fix

--- algo/fifo_push_relabel.py
+++ algo/fifo_push_relabel.py
@@ -1,60 +1,63 @@
-from collections import deque
+from graph import Graph
 
-def initialize_preflow(graph, s):
-    n = graph.number_of_nodes()
-    height = [0]*n
-    excess = [0]*n
-    flow = {}
-
-    height[s] = n
-    for u in graph:
-        for v in graph.adj[u]:
-            flow[(u, v)] = 0
-
-    for v in graph.adj[s]:
-        flow[(s, v)] = graph[s][v]['capacity']
-        flow[(v, s)] = -flow[(s, v)]
-        excess[v] = flow[(s, v)]
-        excess[s] -= flow[(s, v)]
-
-    return height, excess, flow
-
-def push(u, v, excess, flow, graph):
-    delta = min(excess[u], graph[u][v]['capacity'] - flow[(u, v)])
-    flow[(u, v)] += delta
-    flow[(v, u)] -= delta
-    excess[u] -= delta
-    excess[v] += delta
-
-def relabel(u, height, graph, flow):
-    min_height = float('inf')
-    for v in graph.adj[u]:
-        if graph[u][v]['capacity'] - flow[(u, v)] > 0:
-            min_height = min(min_height, height[v])
-    height[u] = min_height + 1
-
-def discharge(u, height, excess, flow, graph, queue,in_queue,s,t):
-    while excess[u] > 0:
-        for v in graph.adj[u]:
-            if graph[u][v]['capacity'] - flow[(u, v)] > 0 and height[u] == height[v] + 1:
-                push(u, v, excess, flow, graph)
-                if not in_queue[v] and v != s and v != t:
-                    queue.append(v)
-                    in_queue[v] = True
-        relabel(u, height, graph, flow)
-
-def fifo_push_relabel_impl(graph, s, t):
-    height, excess, flow = initialize_preflow(graph, s)
-    queue = deque([u for u in graph.adj[s] if u != s and u != t and excess[u] > 0])
-    in_queue = [v in queue for v in graph.nodes]
-
+def fifo_push_relabel(graph, source, sink):
+    queue = []
+    excess = {u: 0 for u in graph.vertices}
+    height = {u: 0 for u in graph.vertices}
+    inQueue = {u: False for u in graph.vertices}
+    height[source] = len(graph.vertices)
+    
+    graph_residual  = graph.copy
+    
+    for v in graph_residual[source]:
+        if graph_residual[source][v]['capacity'] != 0:
+            graph_residual[source][v]['capacity'] = 0
+            graph_residual[v][source]['capacity'] = graph.vertices[source][v]['capacity']
+            excess[v] = graph_residual[v][source]['capacity']
+            if v != sink:
+                queue.append(v)
+                inQueue[v] = True
+    
     while queue:
-        u = queue.popleft()
-        in_queue[u] = False
-        discharge(u, height, excess, flow, graph, queue,in_queue, s, t)
-        if excess[u] > 0:
-            queue.append(u)
-            in_queue[u] = True
+        u = queue.pop(0)
+        inQueue[u] = False
+        relabel(graph_residual, u, height)
+        push(graph_residual, u, excess, height, queue, inQueue, source, sink)
+    
+    return excess[sink]
 
-    max_flow = sum(flow[(s, v)] for v in graph.adj[s])
-    return max_flow
+def relabel(graph, u, height):
+    minHeight = float("inf")
+    for v in graph[u]:
+        if graph[u][v]['capacity'] > 0:
+            minHeight = min(minHeight, height[v])
+            height[u] = minHeight + 1
+
+def push(graph, u, excess, height, queue, in_queue, source, sink):
+    for v in graph[u]:
+        if excess[u] == 0:
+            break
+
+        if graph[u][v]['capacity'] > 0 and height[v] < height[u]:
+            flow = min(excess[u], graph[u][v]['capacity'])
+
+            graph[u][v]['capacity'] -= flow
+            graph[v][u]['capacity'] += flow
+
+            excess[u] -= flow
+            excess[v] += flow
+
+            if not in_queue[v] and v != source and v != sink:
+                queue.append(v)
+                in_queue[v] = True
+
+    if excess[u] != 0:
+        queue.append(u)
+        in_queue[u] = True
+
+
+# source = 1  # источник
+# with open(filename, 'r') as file:
+#     sink, _ = map(int, file.readline().split())
+# max_flow = fifo_push_relabel(my_graph, source, sink)
+# print("Максимальный поток:", max_flow)
